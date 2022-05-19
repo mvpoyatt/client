ee33385c2f2dad23338e7fb513bb2258931035e2
mvpoyatt <poyattm@gmail.com>
2022-05-19 08:52:08 -0400

    Trying to get web3.js to work

--- packages/web/src/pages/landing-page/LandingPage.tsx
+++ packages/web/src/pages/landing-page/LandingPage.tsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import Web3 from 'web3';
+import { AbiItem } from 'web3-utils'
+import { STRINGIFY_ABI, NOTEFACTORY_ABI, NOTEFACTORY_ADDRESS } from '../../config';
 
 function LandingPage() {
-  return <h1>Landing Page</h1>;
+  const [account, setAccount] = useState<any | null>(null);
+  // const [noteFactory, setNoteFactory] = useState<any | null>(null);
+  const [notebookName, setNotebookName] = useState("");
+
+  useEffect(() => {
+    async function load() {
+      // Connect to contract
+      const web3 = new Web3(Web3.givenProvider);
+      const noteFactory = new web3.eth.Contract(STRINGIFY_ABI as AbiItem[], NOTEFACTORY_ADDRESS);
+
+      // const createResponse = await noteFactory.methods._createNotebook("Weird7").call({from: account});
+
+      const response = await noteFactory.methods.getNotebooksForCaller().call({from: account});
+      debugger;
+    }
+
+    load();
+  }, []);
+
+  return (
+    <>
+      <div>
+        Your account is: {account}
+      </div>
+      <div>
+        Your notebook is named: {notebookName}
+      </div>
+    </>
+  );
 }
 
 export default LandingPage;
--- packages/web/src/pages/new-entry-page/NewEntryPage.tsx
+++ packages/web/src/pages/new-entry-page/NewEntryPage.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
 
-function NewEntryPage() {
-  return <h1>New Entry</h1>;
-}
+export default class NewEntryPage extends React.Component<{}, { value: string }> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      value: 'Type new entry here'
+    };
 
-export default NewEntryPage;
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event: any) {
+    this.setState({value: event.target.value});
+  }
+
+  async handleSubmit(event: any) {
+    alert("User submitted");
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          New Entry:
+          <textarea value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
