3cf5dff8ac910d92c2abc0e9ba9b7998bcdf1e1f
mvpoyatt <poyattm@gmail.com>
2022-05-20 13:15:23 -0400

    Switched from web3.js to ethers.js

--- packages/web/src/config.tsx
+++ packages/web/src/config.tsx
@@ -1,9 +1,5 @@
 export const NOTEFACTORY_ADDRESS = '0x5fbdb2315678afecb367f032d93f642f64180aa3';
 
-
-export const STRINGIFY_ABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"uint256","name":"_notebookId","type":"uint256"},{"internalType":"string","name":"_name","type":"string"},{"internalType":"string","name":"_text","type":"string"}],"name":"_createEntry","outputs":[],"stateMutability":"nonpayable","type":"function","signature":"0x8158310c"},{"inputs":[{"internalType":"string","name":"_name","type":"string"}],"name":"_createNotebook","outputs":[],"stateMutability":"nonpayable","type":"function","signature":"0x6bff9b60"},{"inputs":[{"internalType":"uint256","name":"_notebookId","type":"uint256"}],"name":"getEntriesForNotebook","outputs":[{"components":[{"internalType":"uint256","name":"id","type":"uint256"},{"internalType":"uint256","name":"notebookId","type":"uint256"},{"internalType":"uint256","name":"dateCreated","type":"uint256"},{"internalType":"string","name":"name","type":"string"},{"internalType":"string","name":"text","type":"string"}],"internalType":"struct NoteFactory.Entry[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function","constant":true,"signature":"0x82de2e6d"},{"inputs":[],"name":"getNotebooksForCaller","outputs":[{"components":[{"internalType":"uint256","name":"id","type":"uint256"},{"internalType":"string","name":"name","type":"string"}],"internalType":"struct NoteFactory.Notebook[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function","constant":true,"signature":"0x860df1f7"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function","constant":true,"signature":"0x8da5cb5b"}];
-
-
 export const NOTEFACTORY_ABI = 
 [
   {
--- packages/web/src/pages/landing-page/LandingPage.tsx
+++ packages/web/src/pages/landing-page/LandingPage.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
-import Web3 from 'web3';
-import { AbiItem } from 'web3-utils'
-import { STRINGIFY_ABI, NOTEFACTORY_ABI, NOTEFACTORY_ADDRESS } from '../../config';
+import { ethers } from "ethers";
+import { NOTEFACTORY_ABI, NOTEFACTORY_ADDRESS } from '../../config';
 
 function LandingPage() {
   const [account, setAccount] = useState<any | null>(null);
-  // const [noteFactory, setNoteFactory] = useState<any | null>(null);
   const [notebookName, setNotebookName] = useState("");
 
   useEffect(() => {
     async function load() {
+      // Connect to metamask
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const accounts = await provider.send("eth_requestAccounts", []);
+      const signer = await provider.getSigner(accounts[0]);
+      setAccount(signer._address);
+
       // Connect to contract
-      const web3 = new Web3(Web3.givenProvider);
-      const noteFactory = new web3.eth.Contract(STRINGIFY_ABI as AbiItem[], NOTEFACTORY_ADDRESS);
+      const noteFactory = new ethers.Contract(NOTEFACTORY_ADDRESS, NOTEFACTORY_ABI, provider);
+      const noteFactoryWithSigner = noteFactory.connect(signer);
 
-      // const createResponse = await noteFactory.methods._createNotebook("Weird7").call({from: account});
-
-      const response = await noteFactory.methods.getNotebooksForCaller().call({from: account});
-      debugger;
+      // Play with notebooks
+      // await noteFactoryWithSigner._createNotebook("dsfda");
+      const response:any[] = await noteFactoryWithSigner.getNotebooksForCaller();
+      setNotebookName(response[response.length][1]);
     }
 
     load();
